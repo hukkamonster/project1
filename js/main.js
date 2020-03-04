1bac98abd5c0c042c29682c22acc6f7d5f83f6d2
hukkamonster <61619767+hukkamonster@users.noreply.github.com>
2020-03-04 22:50:29 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    
+    // mobile nav
+
+    const navToggle = $("#navToggle");
+    const nav = $("#nav");
+
+    $("#navToggle").on("click", function(event){
+        event.preventDefault();
+
+
+        nav.toggleClass("show");
+    })
+});
